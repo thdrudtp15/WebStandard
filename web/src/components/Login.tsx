1ae66acb1bdbccde0c600a1499731544f5ace424
KyeongSe Song <thdrudtp15@naver.com>
2023-09-06 22:49:18 +0900

    ...로고 찾기 실패..

--- web/src/components/Login.tsx
+++ web/src/components/Login.tsx
@@ -1,5 +1,6 @@
 import "../css/login.css";
 
+import React, { useEffect } from 'react';
 
 
 
@@ -9,10 +10,21 @@ import "../css/login.css";
 function Login(){
 
     
-     
-    
-
-    return <div className="login-wrap" onClick={()=>{}}></div>
+    return <div className="login-wrap" onClick={()=>{}}>
+                    <div className="login-box">
+                        <div className="login-logoBox">
+                        
+                        </div>
+                        <div className="login-subjectBox">로그인</div>
+                        <div className="login-inputBox">
+                            <input type="text" placeholder="아이디"/>
+                        </div>
+                        <div className="login-inputBox">
+                            <input type="password" placeholder="비밀번호"/>
+                        </div>
+                        <button className="login-btn">로그인</button>
+                    </div>
+            </div>
 }
 
 export default Login;
