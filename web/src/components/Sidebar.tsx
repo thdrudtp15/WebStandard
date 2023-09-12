0fa6bdce4808a65f323d35a0e5e910ae186362fa
KyeongSe Song <thdrudtp15@naver.com>
2023-09-12 23:38:08 +0900

    feat : 사이드바 로그인 버튼 기능 추가

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -1,7 +1,9 @@
 import { useSelector,useDispatch } from "react-redux";
 import { onChnageSidebarStatus } from "../store";
+import { onchangeLoginStatus } from "../store";
 import {PiArrowRightBold} from "react-icons/pi";
 import {IoIosArrowUp} from "react-icons/io";
+import {SlLogin} from "react-icons/sl";
 import {useEffect,useState} from "react";
 import "../css/sidebar.css";
 import catgArr from "../data/navbarData";
@@ -14,46 +16,62 @@ function Sidebar(){
     })
     const contentBox = document.querySelectorAll(".sb-catgContentBox");
     const arrow =document.querySelectorAll(".sb-catgArrow");
+    const catgBox = document.querySelectorAll(".sb-catgBox");
 
 
 
 
     function closeSidebar(){
         dispatch(onChnageSidebarStatus(""))
-        if(contentBox instanceof NodeList && arrow instanceof NodeList){
+        if(contentBox instanceof NodeList && arrow instanceof NodeList && catgBox instanceof NodeList){
             for(let i = 0; i < contentBox.length; i++){
                     contentBox[i].classList.remove("openCatg");
                     arrow[i].classList.remove("selectArrow");
+                    catgBox[i].classList.remove("selectCatg");
                 }
             }
         }        
     
+    function onLogin(){
+        dispatch(onChnageSidebarStatus(""))
+        const STO = setTimeout(()=>{
+            dispatch(onchangeLoginStatus("login-wrapActivate"))
+            clearTimeout(STO);
+        },100)
+    }
+
+
 
     return <div className={`sb-wrap ${sbStatus}`}>
                 <div className="sb-closeBtn">
-                    <div onClick={()=>{closeSidebar()}}><PiArrowRightBold /></div>                
+                    <div onClick={()=>{closeSidebar()}}> <PiArrowRightBold /></div>                
                 </div>
                 <div className="sb-contentBox">
+                        <div className="sb-login" onClick={()=>{onLogin()}}>
+                            <div className="sb-catgName">로그인<span className="sb-loginIcon"><SlLogin/></span></div>
+                        </div>
                         {catgArr.map((data,index)=>{
-                            return <Accordian data={data} index={index} contentBox = {contentBox} arrow={arrow}/>
+                            return <Accordian data={data} index={index} contentBox = {contentBox} arrow={arrow} catgBox = {catgBox}/>
                         })}
-                    </div>
+                </div>
             </div> 
 }
 
 export default Sidebar;
 
-function Accordian({data,index,contentBox,arrow} : {data : {name : string, path:string, dropdownMenu : string[]},index :number , contentBox : NodeListOf<Element> , arrow : NodeListOf<Element> }){
+function Accordian({data,index,contentBox,arrow,catgBox} : {data : {name : string, path:string, dropdownMenu : string[]},index :number , contentBox : NodeListOf<Element> , arrow : NodeListOf<Element> ,catgBox : NodeListOf<Element> }){
 
     function AccOpen(index : number){
-        if(contentBox instanceof NodeList && arrow instanceof NodeList){
+        if(contentBox instanceof NodeList && arrow instanceof NodeList && catgBox instanceof NodeList){
             for(let i = 0; i < contentBox.length; i++){
                 if(i === index){
                     contentBox[i].classList.toggle("openCatg");
                     arrow[i].classList.toggle("selectArrow");
+                    catgBox[i].classList.toggle("selectCatg");
                 }else{
                     contentBox[i].classList.remove("openCatg");
                     arrow[i].classList.remove("selectArrow");
+                    catgBox[i].classList.remove("selectCatg");
                 }
             }
         }        
